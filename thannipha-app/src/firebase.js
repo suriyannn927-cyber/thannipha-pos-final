ffa2431ce69542882b114f96aa19d646e225263c
Suriyan <@gmail.comgit config --global user.email suriyannn927@gmail.com>
2026-09-13 05:39:46 +0700

    ลูกนำ้ที่เกินมา

--- thannipha-app/src/firebase.js
+++ thannipha-app/src/firebase.js
@@ -5,7 +5,7 @@ const firebaseConfig = {
   apiKey: "AIzaSyC2jnXV67Y6hyNQhSUJSQ9sZwPG4KHLmlI",
   authDomain: "suriyan-f6309.firebaseapp.com",
   projectId: "suriyan-f6309",
-  storageBucket: "suriyan-f6309.appspot.com",,
+  storageBucket: "suriyan-f6309.appspot.com",
   messagingSenderId: "192670390658",
   appId: "1:192670390658:web:e3ff41ff7bae6a089a179b"
 };
